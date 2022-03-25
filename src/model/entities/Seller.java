dddd758c56a5995b55e76cf61e58f0cd3d3de864
Ligiane <ligiane.alves.souza@gmail.com>
2022-03-25 19:09:30 -0300

    add entity Seller

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,102 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+    private Integer id;
+    private String name;
+    private String email;
+    private Date birthday;
+    private Double Salary;
+
+    private Department department;
+
+    public Seller() {
+
+    }
+
+
+    public Seller(Integer id, String name, String email, Date birthday, Double salary, Department department) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.birthday = birthday;
+        Salary = salary;
+        this.department = department;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Double getSalary() {
+        return Salary;
+    }
+
+    public void setSalary(Double salary) {
+        Salary = salary;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Seller seller = (Seller) o;
+        return id.equals(seller.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Seller{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", birthday=" + birthday +
+                ", Salary=" + Salary +
+                ", department=" + department +
+                '}';
+    }
+}
