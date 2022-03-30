2e0d57343b145981cd9b0bd672cad8d82973463e
Ligiane <ligiane.alves.souza@gmail.com>
2022-03-30 19:35:42 -0300

    add method update, delete

--- src/application/Program.java
+++ src/application/Program.java
@@ -7,9 +7,12 @@ import model.entities.Seller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
@@ -34,5 +37,19 @@ public class Program {
         Seller newSeller = new Seller(null, "Ligiane", "ligi@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update =====");
+        seller = sellerDao.findById(8);
+        seller.setName("Ligiane Domingos");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
+
+        System.out.println("\n=== TEST 6: seller delete =====");
+        System.out.println("Enter id for delete test: ");
+        int id = sc.nextInt();
+        sellerDao.deleteById(id);
+        System.out.println("Delete completed");
+
+        sc.close();
     }
 }
